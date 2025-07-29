cbd8635b66c68d056a652b9fe099b146a85f3281
Jostein24sfj <jostein24sfj@gmail.com>
2025-07-29 10:50:31 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="flex justify-center pt-8">
+        <p className="text-white text-3xl">Luminara Celestis</p>
+        <p>“The light of the heavens”</p>
+      </div>
+    </>
+  );
+}
